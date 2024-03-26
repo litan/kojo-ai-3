d9f7ca5f07b9cacd5672a0624171d5f6ea31d52f
Lalit Pant <pant.lalit@gmail.com>
2024-03-26 15:20:53 +0530

    Bump up installer release number to 0.7.0

---
+++ upgrade-release.sh
@@ -0,0 +1,42 @@
+set -x
+
+rm release-0.7.0/kojo-ai_0.7.0-linux/kojo-ai*.jar
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/classgraph-4.8.60.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/fastutil-8.3.0.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/guava-30.0-jre.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/icu4j-65.1.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/kojo-ai_2.13-0.7.0.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/RoaringBitmap-0.9.25.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/tablesaw-core-0.43.1.jar release-0.7.0/kojo-ai_0.7.0-linux/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/univocity-parsers-2.8.4.jar release-0.7.0/kojo-ai_0.7.0-linux/
+
+rm release-0.7.0/kojo-ai_0.7.0-windows/kojo-ai*.jar
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/classgraph-4.8.60.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/fastutil-8.3.0.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/guava-30.0-jre.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/icu4j-65.1.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/kojo-ai_2.13-0.7.0.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/RoaringBitmap-0.9.25.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/tablesaw-core-0.43.1.jar release-0.7.0/kojo-ai_0.7.0-windows/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/univocity-parsers-2.8.4.jar release-0.7.0/kojo-ai_0.7.0-windows/
+
+rm release-0.7.0/kojo-ai_0.7.0-macosx-arm64/kojo-ai*.jar
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/classgraph-4.8.60.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/fastutil-8.3.0.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/guava-30.0-jre.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/icu4j-65.1.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/kojo-ai_2.13-0.7.0.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/RoaringBitmap-0.9.25.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/tablesaw-core-0.43.1.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/univocity-parsers-2.8.4.jar release-0.7.0/kojo-ai_0.7.0-macosx-arm64/
+
+rm release-0.7.0/kojo-ai_0.7.0-macosx-x86/kojo-ai*.jar
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/classgraph-4.8.60.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/fastutil-8.3.0.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/guava-30.0-jre.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/icu4j-65.1.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/kojo-ai_2.13-0.7.0.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/RoaringBitmap-0.9.25.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/tablesaw-core-0.43.1.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+cp /home/lalit/.kojo/extension/kojo-ai_0.7.0-linux/univocity-parsers-2.8.4.jar release-0.7.0/kojo-ai_0.7.0-macosx-x86/
+
